04f8e8eb2282299102d8b4a643e3e24d2ff3f637
Neamar <neamar@neamar.fr>
2015-01-27 19:05:22 +0100

    Removing components

--- app/src/main/java/bealder/natixis/WeatherActivity.java
+++ app/src/main/java/bealder/natixis/WeatherActivity.java
@@ -1,54 +1,12 @@
 package bealder.natixis;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.widget.TextView;
 
 
-public class WeatherActivity extends SlideActivity {
-		/**
-		 * The number of weather station to show
-		 */
-		private final int NUM_PAGES = DownloadWeatherService.STATIONS.length;
-
+public class WeatherActivity extends Activity {
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
-				super.onCreate(savedInstanceState);
-				setContentView(R.layout.activity_weather);
-
-				new MixinMenuActivity().addMenuListeners(this);
-
-				TextView dateText = (TextView) findViewById(R.id.date);
-
-				String dateString = DownloadWeatherService.getCurrentDateFormatted();
-
-				dateString = dateString.substring(0, 1).toUpperCase() + dateString.substring(1).toLowerCase();
-				dateText.setText(dateString);
-
-				initPager(new ScreenSlidePagerAdapter(getSupportFragmentManager()));
-		}
-
-
-		protected class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-				public ScreenSlidePagerAdapter(FragmentManager fm) {
-						super(fm);
-				}
-
-				@Override
-				public Fragment getItem(int position) {
-						Bundle bundle = new Bundle();
-						bundle.putInt("page", position);
-
-						Fragment page = new WeatherSlideFragment();
-						page.setArguments(bundle);
-						return page;
-				}
-
-				@Override
-				public int getCount() {
-						return NUM_PAGES;
-				}
+            super.onCreate(savedInstanceState);
 		}
 }
